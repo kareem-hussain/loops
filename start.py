7e6eb74c36d8c5a44cdc16f462793938c53377cd
kareem <kzbaba3@gmail.com>
2025-10-23 20:21:33 +0500

    Added project files

--- start.py
+++ start.py
@@ -1,4 +1,22 @@
-a=78
-while(a<134):
-    print(a)
-    a+=1
+# a=78
+# while(a<134):
+#     print(a)
+#     a+=1
+# for i in range(100):
+#     print(i)
+#     if i==35:
+#         break
+
+
+
+# for i in range(0,100,8):
+#     print(i)
+        
+# for i in range(5):
+#     print(i)
+#     if i==3:
+#         continue
+    
+n=int(input("enter number"))
+for i in range(11):
+    print(f"6x{i}={n*i}")
